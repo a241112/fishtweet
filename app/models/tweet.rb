e5356c10f96bc5a3f093ce07a45cbc25741d5a74
a241112 <af.vly-gtr.1112@outlook.jp>
2020-11-06 17:17:17 +0900

    アソシエーションの設定

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,8 @@
 class Tweet < ApplicationRecord
+  belongs_to_active_hash :quantity
+  belongs_to_active_hash :size
+  belongs_to_active_hash :weather
+  belongs_to_active_hash :wind
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+
+  has_many :tweets
 end
