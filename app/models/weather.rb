f7b82cfb80091c2e2b65da385c0b875e2bb57bb3
a241112 <af.vly-gtr.1112@outlook.jp>
2020-11-06 13:57:45 +0900

    プルダウンリストの作成

---
+++ app/models/weather.rb
@@ -0,0 +1,10 @@
+class Weather < ActiveHash::Base
+  self.data = [
+    { id: 0, name: '---' },
+    { id: 1, name: '晴れ' },
+    { id: 2, name: 'くもり' },
+    { id: 3, name: '雨' },
+    { id: 4, name: 'その他' },
+  ]
+
+end
