e85c531e094bab26a539ee2e5410586108f1af67
a241112 <af.vly-gtr.1112@outlook.jp>
2020-11-08 06:54:04 +0900

    バリデーションの設定

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,8 +1,12 @@
 class Tweet < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :quantity
   belongs_to_active_hash :size
   belongs_to_active_hash :weather
   belongs_to_active_hash :wind
-
+  
+  has_many_attached :image
   belongs_to :user
+
+  validates :image, :type_name, :date, :wind, :feed, :address, presence: true
 end
