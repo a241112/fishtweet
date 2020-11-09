de7fd76ca5168eb3af6c144f390b0e70db079c25
a241112 <af.vly-gtr.1112@outlook.jp>
2020-11-09 18:10:17 +0900

    バリデーションの設定

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -9,4 +9,12 @@ class Tweet < ApplicationRecord
   belongs_to :user
 
   validates :image, :type_name, :date, :wind, :feed, :address, presence: true
+  validates :type_name, format: { with: /\A[ァ-ン]+\z/, message: "は全角カタカナで入力してください" }
+  validates :feed, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/,  message: "は全角文字で入力してください" }
+  with_options numericality: { other_than: 0 , message: "を選択してください" } do
+    validates :quantity_id
+    validates :size_id
+    validates :weather_id
+    validates :wind_id
+  end
 end
