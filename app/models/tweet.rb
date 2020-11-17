6b374e5fcd85c8ddfbb56048dc0b8ef4ebb09fb9
a241112 <af.vly-gtr.1112@outlook.jp>
2020-11-18 03:01:25 +0900

    バリデーションの修正

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -12,8 +12,8 @@ class Tweet < ApplicationRecord
   before_validation :geocode
 
   validates :image, :type_name, :datetime, :wind, :feed, :address, :latitude, :longitude, presence: true
-  validates :type_name, format: { with: /\A[ァ-ン]+\z/, message: "は全角カタカナで入力してください" }
-  validates :feed, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/,  message: "は全角文字で入力してください" }
+  validates :type_name, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
+  validates :feed, format: { with: /\A[ぁ-んァ-ン一-龥]/,  message: "は全角文字で入力してください" }
   with_options numericality: { other_than: 0 , message: "を選択してください" } do
     validates :quantity_id
     validates :size_id
